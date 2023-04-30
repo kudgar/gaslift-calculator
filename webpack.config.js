5ceca198c9ea5d3b3656ebd8f6b8653f62bc610b
Ilya Dobatovkin <ramon_mattlov@vulong-development.ru>
2023-04-30 09:45:17 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name][contenthash].js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+}
